ec2c7b7e52c76843a8dd986ea272279d7fdb408a
Nikita <n.plaksin8@gmail.com>
2021-03-10 18:00:22 +0300

    Update data_configs.py

--- configs/data_configs.py
+++ configs/data_configs.py
@@ -10,32 +10,11 @@ DATASETS = {
 		'test_source_root': dataset_paths['celeba_test'],
 		'test_target_root': dataset_paths['celeba_test'],
 	},
-	'ffhq_frontalize': {
-		'transforms': transforms_config.FrontalizationTransforms,
-		'train_source_root': dataset_paths['ffhq'],
-		'train_target_root': dataset_paths['ffhq'],
-		'test_source_root': dataset_paths['celeba_test'],
-		'test_target_root': dataset_paths['celeba_test'],
-	},
-	'celebs_sketch_to_face': {
-		'transforms': transforms_config.SketchToImageTransforms,
-		'train_source_root': dataset_paths['celeba_train_sketch'],
-		'train_target_root': dataset_paths['celeba_train'],
-		'test_source_root': dataset_paths['celeba_test_sketch'],
-		'test_target_root': dataset_paths['celeba_test'],
-	},
-	'celebs_seg_to_face': {
-		'transforms': transforms_config.SegToImageTransforms,
-		'train_source_root': dataset_paths['celeba_train_segmentation'],
-		'train_target_root': dataset_paths['celeba_train'],
-		'test_source_root': dataset_paths['celeba_test_segmentation'],
-		'test_target_root': dataset_paths['celeba_test'],
-	},
-	'celebs_super_resolution': {
-		'transforms': transforms_config.SuperResTransforms,
-		'train_source_root': dataset_paths['celeba_train'],
-		'train_target_root': dataset_paths['celeba_train'],
-		'test_source_root': dataset_paths['celeba_test'],
-		'test_target_root': dataset_paths['celeba_test'],
-	},
+	'panel_to_pixel': {
+		'transforms': transforms_config.EncodeTransforms,
+		'train_source_root': dataset_paths['panel'],
+		'train_target_root': dataset_paths['pixel'],
+		'test_source_root': dataset_paths['panel'],
+		'test_target_root': dataset_paths['pixel'],
+	}
 }
