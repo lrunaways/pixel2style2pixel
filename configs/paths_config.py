573df2a562320683b1911b45e9eac0f6d3fc8f53
Nikita <n.plaksin8@gmail.com>
2021-03-10 18:11:24 +0300

    Update paths_config.py

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -6,14 +6,12 @@ dataset_paths = {
 	'celeba_train_segmentation': '',
 	'celeba_test_segmentation': '',
 	'ffhq': '',
+	'panel': '"my_data/pixelated panelkas p4 c-1 256x256",
+	'pixel': '"my_data/good_quality_png_256_cuts"
 }
 
 model_paths = {
 	'stylegan_ffhq': 'pretrained_models/stylegan2-ffhq-config-f.pt',
 	'ir_se50': 'pretrained_models/model_ir_se50.pth',
-	'circular_face': 'pretrained_models/CurricularFace_Backbone.pth',
-	'mtcnn_pnet': 'pretrained_models/mtcnn/pnet.npy',
-	'mtcnn_rnet': 'pretrained_models/mtcnn/rnet.npy',
-	'mtcnn_onet': 'pretrained_models/mtcnn/onet.npy',
-	'shape_predictor': 'shape_predictor_68_face_landmarks.dat'
+	'pixel': '../networks/pixelation-003-1080.pkl'
 }
